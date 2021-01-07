8e99c89afe403dd9a43ac358324a0e9e78f8c097
cheng.chen <cheng.chen@orange.com>
2021-01-07 11:50:09 +0800

    change port

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-app.set('port', process.env.PORT || 3000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
